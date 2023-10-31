cbb96b1c31c59c32633e701e2fe8fbe85b7edefb
preciousnwankwo <realpreciousnwankwo@gmail.com>
2023-10-31 15:38:49 -0700

    Changes Made

--- myfunctions.c
+++ myfunctions.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
-
-/************************* PRINT CHAR ONN THE TERMINAL *************************/
+/************************* PRINT CHAR *************************/
 
 /**
  * print_char - Prints a char
@@ -20,54 +19,7 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-
-/************************* PRINT BINARY ON THE TERMINAL *************************/
-/**
- * print_binary - Prints an unsigned number
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: Will get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Numbers of char printed.
- */
-int print_binary(va_list types, char buffer[],
-        int flags, int width, int precision, int size)
-{
-        unsigned int n, m, i, sum;
-        unsigned int a[32];
-        int count;
-
-        UNUSED(buffer);
-        UNUSED(flags);
-        UNUSED(width);
-        UNUSED(precision);
-        UNUSED(size);
-
-        n = va_arg(types, unsigned int);
-        m = 2147483648; /* (2 ^ 31) */
-        a[0] = n / m;
-        for (i = 1; i < 32; i++)
-        {
-                m /= 2;
-                a[i] = (n / m) % 2;
-        }
-        for (i = 0, sum = 0, count = 0; i < 32; i++)
-        {
-                sum += a[i];
-                if (sum || i == 31)
-                {
-                        char z = '0' + a[i];
-
-                        write(1, &z, 1);
-                        count++;
-                }
-        }
-        return (count);
-}
-
-/************************* PRINT A STRING ON THE TERMINAL *************************/
+/************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -122,7 +74,7 @@ int print_string(va_list types, char buffer[],
 
 	return (write(1, str, length));
 }
-/************************* PRINT PERCENT SIGN ON THE TERMINAL *************************/
+/************************* PRINT PERCENT SIGN *************************/
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -145,7 +97,7 @@ int print_percent(va_list types, char buffer[],
 	return (write(1, "%%", 1));
 }
 
-/************************* PRINT INT ON THE  TERMINAL *************************/
+/************************* PRINT INT *************************/
 /**
  * print_int - Print int
  * @types: Lista of arguments
@@ -187,4 +139,50 @@ int print_int(va_list types, char buffer[],
 	i++;
 
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+}
+
+/************************* PRINT BINARY *************************/
+/**
+ * print_binary - Prints an unsigned number
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Numbers of char printed.
+ */
+int print_binary(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
 }
